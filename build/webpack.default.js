70a206c526f3c3941f677d155a4b5220536bd84a
zhangpeng <zhangpeng@htmimi.com>
2017-11-28 18:37:00 +0800

    1

--- build/webpack.default.js
+++ build/webpack.default.js
@@ -21,11 +21,46 @@ module.exports = {
               ],
               plugins: [
                 'transform-runtime', // 避免辅助代码重复引入
-                'transform-decorators-legacy'
+                'transform-decorators-legacy', // 支持es7的装饰者模式
+                // 'react-css-modules'  css样式模块化
               ],
               cacheDirectory: true // 编译结果缓存到文件系统，提升编辑效率
             }
           }
+        ]
+      }, {
+        test: /\.(scss|sass)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader?modules'
+          }, {
+            loader: 'sass-loader'
+          },
+          // {
+          //   loader: 'postcss-loader',
+          //   options: {
+          //     ident: 'postcss',
+          //     plugins: (loader) => [
+          //       require('postcss-import')(),
+          //       require('postcss-nested')(),
+          //       require('postcss-cssnext')(),
+          //       require('autoprefixer')([
+          //         'Android 2.3',
+          //         'Android >= 4',
+          //         'Chrome >= 35',
+          //         'Firefox >= 31',
+          //         'Explorer >= 8',
+          //         'iOS >= 7',
+          //         'Opera >= 12',
+          //         'Safari >= 7.1'
+          //       ]),
+          //       require('cssnano')(),
+          //       require('postcss-px2rem')({remUnit: 20})
+          //     ]
+          //   }
+          // }
         ]
       }
     ]
--- src/views/mine-login/MineLoginView.jsx
+++ src/views/mine-login/MineLoginView.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import CSSModules from 'react-css-modules'
 import {actions} from './MineLoginReducer'
+import styles from './MineLoginView.scss'
 
 @connect(state => {
   return {}
 }, dispatch => {
   return {}
 })
+@CSSModules(styles)
 class MineLoginView extends React.Component {
   render() {
-    return (<div>Login</div>)
+    return (<div styleName='foo'>Login</div>)
   }
 }
 
