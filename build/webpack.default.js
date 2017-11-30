20c1770ab60d59cead241bb3097c3ebd3c765c58
zhangpeng <zhangpeng@htmimi.com>
2017-11-30 17:31:25 +0800

    1

--- build/webpack.default.js
+++ build/webpack.default.js
@@ -20,11 +20,25 @@ module.exports = {
                 'react', 'env', 'stage-0'
               ],
               plugins: [
-                'transform-runtime', // 避免辅助代码重复引入
-                'transform-decorators-legacy', // 支持es7的装饰者模式
-                // 'react-css-modules'  css样式模块化
+                // 避免辅助代码重复引入
+                'transform-runtime',
+                // 支持es7的装饰者模式
+                'transform-decorators-legacy',
+                [
+                  // 自动匹配样式，并模块化
+                  'react-css-modules', {
+                    // 匹配文件类型
+                    'filetypes': {
+                      '.scss': {
+                        // 解析语法
+                        'syntax': 'postcss-scss'
+                      }
+                    }
+                  }
+                ]
               ],
-              cacheDirectory: true // 编译结果缓存到文件系统，提升编辑效率
+              // 编译结果缓存到文件系统，提升编辑效率
+              cacheDirectory: true
             }
           }
         ]
@@ -34,33 +48,25 @@ module.exports = {
           {
             loader: 'style-loader'
           }, {
-            loader: 'css-loader?modules'
+            loader: 'css-loader?importLoader=1&modules&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
           }, {
             loader: 'sass-loader'
-          },
-          // {
-          //   loader: 'postcss-loader',
-          //   options: {
-          //     ident: 'postcss',
-          //     plugins: (loader) => [
-          //       require('postcss-import')(),
-          //       require('postcss-nested')(),
-          //       require('postcss-cssnext')(),
-          //       require('autoprefixer')([
-          //         'Android 2.3',
-          //         'Android >= 4',
-          //         'Chrome >= 35',
-          //         'Firefox >= 31',
-          //         'Explorer >= 8',
-          //         'iOS >= 7',
-          //         'Opera >= 12',
-          //         'Safari >= 7.1'
-          //       ]),
-          //       require('cssnano')(),
-          //       require('postcss-px2rem')({remUnit: 20})
-          //     ]
-          //   }
-          // }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: (loader) => [
+                // 解决样式文件import路径问题
+                require('postcss-import')(),
+                // 添加-webkit等前缀
+                require('autoprefixer')(),
+                // 可以保证生产环境的代码尽量小
+                require('cssnano')(),
+                // px转为rem
+                require('postcss-px2rem')({remUnit: 20})
+              ]
+            }
+          }
         ]
       }
     ]
--- src/views/mine-login/MineLoginView.jsx
+++ src/views/mine-login/MineLoginView.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import CSSModules from 'react-css-modules'
 import {actions} from './MineLoginReducer'
-import styles from './MineLoginView.scss'
+import './MineLoginView.scss'
 
 @connect(state => {
   return {}
 }, dispatch => {
   return {}
 })
-@CSSModules(styles)
 class MineLoginView extends React.Component {
   render() {
     return (<div styleName='foo'>Login</div>)
