26ea31f8a5c5f8040fbb3d2b8127e87935273d21
zhangpeng <zhangpeng@htmimi.com>
2017-12-01 09:36:52 +0800

    1

---
+++ src/views/mine-register/view.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import './view.scss'
+
+@connect(state => {
+  console.log(state);
+  return {}
+}, dispatch => {
+  return {}
+})
+class View extends React.Component {
+  render() {
+    return (<div>Register</div>)
+  }
+}
+
+export default View
