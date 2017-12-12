a6d30f2e79411837e9eb83d0edeb1952d6f27476
zhangpeng <zhangpeng@htmimi.com>
2017-12-12 16:25:50 +0800

    1

--- src/views/mine/login/view.jsx
+++ src/views/mine/login/view.jsx
@@ -6,6 +6,8 @@ import logo from '../../../assets/images/logo.png'
 import {actions} from './reducer'
 import {action1} from './actions.async'
 // import {LOGIN, LOGIN_SUCCESS, LOGIN_ERROR} from '../../../../reducers/login/actions.names'
+import {List, InputItem, WhiteSpace, Button} from 'antd-mobile'
+// import {createForm} from 'rc-form'
 
 @connect(state => {
   return {b: state.mineLogin.list, counter: state.mineLogin.counter, cc: state.login.login}
@@ -27,6 +29,12 @@ class View extends Component {
     return (<div className='gfoo'>
       <NavBar title='登录'/>
       <img styleName='logo' src={logo} onClick={() => action1()}/>
+      <InputItem clear="clear" placeholder="请输入手机号"></InputItem>
+      <img src={logo} width="100" height="64"/>
+      <InputItem clear="clear" placeholder="请输入图片验证码"></InputItem>
+      <InputItem clear="clear" placeholder="请输入6位短信验证码"></InputItem>
+      <Button type="primary">获取验证码</Button>
+      <Button type="primary">确认</Button>
     </div>)
   }
 }
